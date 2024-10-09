aaeee505aaf55decef1b55112835c352ec664dfe
Garvit Singla <garvitsingla2005@gmail.com>
2024-10-09 20:26:14 +0530

    import

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var account = document.querySelector("#account");
 var right = document.querySelector("#right");
 var closeOverlay = document.querySelector("#closeOverlay"); 
 var upgrade1 = document.querySelector("#upgrade1"); 
+var playbtn = document.querySelector(".playbutton")
+var audioPlayer = document.querySelector("#audio-player")
+
 
 upgrade.addEventListener("click", function() {
     window.location.href = "subscription.html";
@@ -18,4 +21,14 @@ closeOverlay.addEventListener("click", function() {
 
 upgrade1.addEventListener("click", function() {
     window.location.href = "subscription.html"; 
+});
+
+playbtn.addEventListener('click', () => {
+    if (audioPlayer.paused) {
+        audioPlayer.play();
+        playbtn.innerHTML = '<i class="ri-pause-circle-fill"></i>'; // Change button text to 'Pause' when playing
+    } else {
+        audioPlayer.pause();
+        playbtn.innerHTML = '<i class="ri-play-circle-fill"></i>'; // Change button text to 'Play' when paused
+    }
 });
