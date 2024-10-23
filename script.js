bdebfc7f92fb5736f17d768ae5389c489c974bc5
Garvit Singla <garvitsingla2005@gmail.com>
2024-10-23 16:44:25 +0530

    done

--- script.js
+++ script.js
@@ -5,27 +5,37 @@ var closeOverlay = document.querySelector("#closeOverlay");
 var upgrade1 = document.querySelector("#upgrade1"); 
 var playbtn = document.querySelector(".playbutton")
 var audioPlayer = document.querySelector("#audio-player")
+var playerimage = document.querySelector("#playerimage")
 
+const data = [
+    {song:"Starboy",img:"images/starboy.jpg"}, // Only keep the image path, no need to include the <img> tag here
+];
 
+// Open subscription page
 upgrade.addEventListener("click", function() {
     window.location.href = "subscription.html";
 });
 
+// Open account overlay
 account.addEventListener("click", function() {
     right.classList.add("overlayin");
 });
 
+// Close account overlay
 closeOverlay.addEventListener("click", function() {
     right.classList.remove("overlayin");
 });
 
+// Open subscription page from another button
 upgrade1.addEventListener("click", function() {
     window.location.href = "subscription.html"; 
 });
 
+// Play or pause audio and update UI
 playbtn.addEventListener('click', () => {
     if (audioPlayer.paused) {
         audioPlayer.play();
+        playerimage.src = data[0].img; // Set the image src when playing
         playbtn.innerHTML = '<i class="ri-pause-circle-fill"></i>'; // Change button text to 'Pause' when playing
     } else {
         audioPlayer.pause();
